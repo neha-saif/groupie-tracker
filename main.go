5c3b725d23ccb3eb837064ad252df8e13a303fe1
neha-saif <160529296+neha-saif@users.noreply.github.com>
2024-10-13 09:54:43 +0000

    Add files via upload

--- main.go
+++ main.go
@@ -2,95 +2,131 @@ package main
 
 import (
 	"fmt"
-	"groupie/functions"
+	//"strconv"
+	//"groupie/functions"
+	"io"
+
 	//"image/jpeg"
 	//"image"
+	"encoding/json"
 	"net/http"
-	//"os"
-	"strings"
+
+	//"strings"
 	"text/template"
 )
 
-
 type Data struct {
-	Artist      []string
-    Members     []string
-    Albums      []string
-    AlbumYears  []string
-    Locations   []string
-    ConcertDates []string
-	ImageCover string
+	ID           int      `json:"id"`
+	Image        string   `json:"image"`
+	Artist       string   `json:"name"`
+	Members      []string `json:"members"`
+	AlbumYear    int      `json:"creationDate"`
+	Album1       string   `json:"firstAlbum"`
+	Locations    []string `json:"locations"`
+	ConcertDates []string `json:"concertDates"`
+	RelUrl       string   `json:"relations"`
 }
 
-
-func main () {
-
-// handler functions
-http.HandleFunc("/", homepage)
-http.HandleFunc("/result", result)
-http.Handle("/images/", http.StripPrefix("/images/", http.FileServer(http.Dir("./images"))))
-http.ListenAndServe(":8080", nil)
-
+type Origin struct {
+	Name string
+	URL  string
 }
-func homepage (w http.ResponseWriter, r *http.Request) {
-// where form value is collected for artist name annd fed innto the relavent funnction
-//ie- artist = r.FormValue = mumford
-t, err := template.ParseFiles("index.html")
+
+func main() {
+
+	// handler functions
+	http.HandleFunc("/", homepage)
+	http.HandleFunc("/result", result)
+	// http.Handle("/images/", http.StripPrefix("/images/", http.FileServer(http.Dir("./images"))))
+	http.ListenAndServe(":8080", nil)
+}
+
+func LoadData() ([]Data, error) {
+	url := "https://groupietrackers.herokuapp.com/api/artists"
+	response, err := http.Get(url)
 	if err != nil {
-		http.Error(w, "Error parsing html", http.StatusInternalServerError)
-		return
+		fmt.Println("Error getting response from url")
 	}
 
-	// execute the HTML template
-	err = t.Execute(w, nil)
+	defer response.Body.Close()
+
+	body, err := io.ReadAll(response.Body)
 	if err != nil {
-		http.Error(w, "Error executing template", http.StatusInternalServerError)
+		fmt.Println("Error cannot store get resposne in body")
 	}
+
+	var character []Data
+	err = json.Unmarshal(body, &character)
+	if err != nil {
+		fmt.Print("Error storing body in address of character")
+	}
+
+	return character, nil
 }
 
-// different function for each artist 
-func result(w http.ResponseWriter, r *http.Request) {
-	fileName := "artists/"+r.FormValue("artist")
-fmt.Println(fileName)
-	fileLines := functions.Read(fileName) 
-	//img , _ := LoadImage(r.FormValue("artist"))
-	fileNameI :="/images/"+r.FormValue("artist")+".jpeg"
-name := Data{
-Artist : strings.Split(fileLines[0],","),
-Members : strings.Split(fileLines[1],","),
-Albums : strings.Split(fileLines[2],","),
-	AlbumYears : strings.Split(fileLines[3],","),
-	Locations : strings.Split(fileLines[4],","),
-	ConcertDates : strings.Split(fileLines[5],","),
-	ImageCover: fileNameI, 
-}	
-fmt.Println(name.ImageCover)
-fmt.Println(name.Albums)
-	// Parse the HTML template again for the resultpage
+func homepage(w http.ResponseWriter, r *http.Request) {
+	// where form value is collected for artist name annd fed innto the relavent funnction
+	//ie- artist = r.FormValue = mumford
+
+	character, _ := LoadData()
+	for i := 0; i < 52; i++ {
+		character[i] = Data{
+			ID:           character[i].ID,
+			Image:        character[i].Image,
+			Artist:       character[i].Artist,
+			Members:      character[i].Members,
+			AlbumYear:    character[i].AlbumYear,
+			Album1:       character[i].Album1,
+			Locations:    character[i].Locations,
+			ConcertDates: character[i].ConcertDates,
+			RelUrl:       character[i].RelUrl,
+		}
+
+	}
 	t, err := template.ParseFiles("index.html")
 	if err != nil {
 		http.Error(w, "Error parsing html", http.StatusInternalServerError)
 		return
 	}
 
-	// Render the template with the result
-	err = t.Execute(w, name)
+	
+	err = t.Execute(w, character)
+	if err != nil {
+		http.Error(w, "Error executing template", http.StatusInternalServerError)
+	}
+
+}
+
+func result(w http.ResponseWriter, r *http.Request) {
+	artId := r.FormValue("artist")
+	fmt.Println(r.FormValue("artist"))
+	fmt.Println(artId)
+
+	character, _ := LoadData()
+	for i := 0; i < 52; i++ {
+		character[i] = Data{
+			ID:           character[i].ID,
+			Image:        character[i].Image,
+			Artist:       character[i].Artist,
+			Members:      character[i].Members,
+			AlbumYear:    character[i].AlbumYear,
+			Album1:       character[i].Album1,
+			Locations:    character[i].Locations,
+			ConcertDates: character[i].ConcertDates,
+			RelUrl:       character[i].RelUrl,
+		}
+	}
+
+	
+	t, err := template.ParseFiles("index.html")
+	if err != nil {
+		http.Error(w, "Error parsing html", http.StatusInternalServerError)
+		return
+	}
+
+
+	err = t.Execute(w, character)
 	if err != nil {
 		http.Error(w, "Error executing template", http.StatusInternalServerError)
 	}
 }
-
-// func  LoadImage (name string) (image.Image, error){
-// 	file, err := os.Open("/images/"+name+".jpeg")
-// 	fmt.Println("/images/"+name+".jpeg")
-// 	if err !=nil{
-// 		return nil, err
-// 	}
-// 	defer file.Close()
-
-// 	ImageCover, err := jpeg.Decode(file)
-// 	if err != nil{
-// 		return nil, err
-// 	}
-// 	return ImageCover, nil
-// }
