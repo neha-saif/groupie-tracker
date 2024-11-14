3c03784e2cc809e5b811915c7b81a1b1f3df4d08
neha-saif <160529296+neha-saif@users.noreply.github.com>
2024-11-14 18:26:57 +0300

    Add files via upload

--- main.go
+++ main.go
@@ -25,16 +25,25 @@ type Data struct {
 	Members      []string `json:"members"` //
 	AlbumYear    int      `json:"creationDate"`
 	Album1       string   `json:"firstAlbum"`
-	Locations    string `json:"locations"`//
-	ConcertDates string `json:"concertDates"`//
+	Locations    []string `json:"locations"`    //
+	ConcertDates string   `json:"concertDates"` //
 	RelUrl       string   `json:"relations"`
-
 }
 
-type UrellesData struct {
-	Locations    string `json:"locations"`//
-	ConcertDates string `json:"concertDates"`//
-	RelUrl       string   `json:"relations"`
+type Urelles struct {
+	ID             int `json:"id"`
+	DatesLocations [][]string `json:"datesLocations"`
+}
+
+type Final struct {
+	ID           int    `json:"id"`
+	Image        string `json:"image"`
+	Artist       string `json:"name"`
+	Members      string `json:"members"` //
+	AlbumYear    string `json:"creationDate"`
+	Album1       string `json:"firstAlbum"`
+	Locations    string `json:"locations"`    //
+	ConcertDates string `json:"concertDates"` //
 }
 
 func main() {
@@ -42,7 +51,7 @@ func main() {
 	// handler functions
 	http.HandleFunc("/", homepage)
 	http.HandleFunc("/result", result)
-	// http.Handle("/images/", http.StripPrefix("/images/", http.FileServer(http.Dir("./images"))))
+	http.Handle("/style/", http.StripPrefix("/style/", http.FileServer(http.Dir("style"))))
 	http.ListenAndServe(":8080", nil)
 }
 
@@ -84,7 +93,6 @@ func homepage(w http.ResponseWriter, r *http.Request) {
 			Locations:    character[i].Locations,
 			ConcertDates: character[i].ConcertDates,
 			RelUrl:       character[i].RelUrl,
-	
 		}
 
 	}
@@ -94,7 +102,6 @@ func homepage(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	
 	err = t.Execute(w, character)
 	if err != nil {
 		http.Error(w, "Error executing template", http.StatusInternalServerError)
@@ -109,9 +116,9 @@ func result(wr http.ResponseWriter, r *http.Request) {
 	fmt.Println(artId)
 	fmt.Println(reflect.TypeOf(artId))
 
-	character, err := LoadData("https://groupietrackers.herokuapp.com/api/artists") 
+	character, err := LoadData("https://groupietrackers.herokuapp.com/api/artists")
 	if err != nil {
-		fmt.Println("err:",err)
+		fmt.Println("err:", err)
 	}
 
 	// i, err := strconv.Atoi(artId)
@@ -125,29 +132,54 @@ func result(wr http.ResponseWriter, r *http.Request) {
 		http.Error(wr, "Invalid artist ID", http.StatusBadRequest)
 		return
 	}
-i := iint - 1
-	fmt.Println("i:",i)
-		character[i] = Data{
-			ID:           character[i].ID,
-			Image:        character[i].Image,
-			Artist:       character[i].Artist,
-			Members:      character[i].Members,
-			AlbumYear:    character[i].AlbumYear,
-			Album1:       character[i].Album1,
-			Locations:    character[i].Locations,
-			ConcertDates: character[i].ConcertDates,
-			RelUrl:       character[i].RelUrl,
-		}
-	
-fmt.Println("char:",character[i])
-	CharData := character[i]
+	i := iint - 1
+	fmt.Println("i:", i)
+	character[i] = Data{
+		ID:           character[i].ID,
+		Image:        character[i].Image,
+		Artist:       character[i].Artist,
+		Members:      character[i].Members,
+		AlbumYear:    character[i].AlbumYear,
+		Album1:       character[i].Album1,
+		Locations:    character[i].Locations,
+		ConcertDates: character[i].ConcertDates,
+		RelUrl:       character[i].RelUrl,
+	}
+
+	members := ""
+	clocations := ""
+	for j, _ := range character[i].Members {
+		members += character[i].Members[j] + ", "
+	}
+
+	CharData, _ := LoadData("https://groupietrackers.herokuapp.com/api/relation")
+
+	CharData[i] = Urelles{
+		ID:             CharData[i].ID,
+		DatesLocations: CharData[i].DatesLocations,
+	}
+
+	for j, _ := range CharData.DatesLocations {
+		clocations += CharData.DatesLocations[j] + ", "
+	}
+
+	fmt.Println("concerrtd:", concertdates)
+	FFinal := Final{
+		ID:           character[i].ID,
+		Image:        character[i].Image,
+		Artist:       character[i].Artist,
+		Members:      members,
+		ConcertDates: clocations,
+	}
+
+	fmt.Println("charM:", character[i].Members[0])
 	t, err := template.ParseFiles("result.html")
 	if err != nil {
 		http.Error(wr, "Error parsing html", http.StatusInternalServerError)
 		return
 	}
 
-	err = t.Execute(wr,CharData)
+	err = t.Execute(wr, FFinal)
 	if err != nil {
 		http.Error(wr, "Error executing template ya", http.StatusInternalServerError)
 		return
